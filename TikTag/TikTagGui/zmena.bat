01965edb2c232cd592f07fef87283e488b645b9a
Marek Saloň <32681232+xsalon@users.noreply.github.com>
2019-06-19 22:05:50 +0200

    Delete zmena.bat

--- TikTag/TikTagGui/zmena.bat
+++
@@ -1,6 +0,0 @@
-C:\Users\Marek\AppData\Local\Programs\Python\Python36-32\Scripts\pyuic5 -x views\mainWindow.ui -o Ui_MainWindow.py
-C:\Users\Marek\AppData\Local\Programs\Python\Python36-32\Scripts\pyuic5 -x views\urlDialog.ui -o Ui_urlDialog.py
-C:\Users\Marek\AppData\Local\Programs\Python\Python36-32\Scripts\pyuic5 -x views\pathDialog.ui -o Ui_pathDialog.py
-C:\Users\Marek\AppData\Local\Programs\Python\Python36-32\Scripts\pyuic5 -x views\tagFileDialog.ui -o Ui_tagFileDialog.py
-C:\Users\Marek\AppData\Local\Programs\Python\Python36-32\Scripts\pyuic5 -x views\settingsDialog.ui -o Ui_settingsDialog.py
-C:\Users\Marek\AppData\Local\Programs\Python\Python36-32\Scripts\pyrcc5 images.qrc -o C:\Users\Marek\source\repos\TikTag\TikTag\images_rc.py
